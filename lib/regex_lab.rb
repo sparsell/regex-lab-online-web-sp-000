803a50747253b8e38fae3ff9fe7806a09063c090
Sonja Parsell <hello@sparsell.com>
2020-03-20 17:23:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,11 +17,15 @@ end
 #returns an array of words that are five letters long
 def words_five_letters_long(text)
   text.scan(/\b[a-zA-Z]{5}\b/)
-
 end
 
+#Returns true for text startign with a Capital letter and ending with punctuation
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+if text.match(/....)
+  return true
+else 
+  return false
+end
 end
 
 def valid_phone_number?(phone)
