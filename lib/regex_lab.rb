ec0d93fe75d4ac7544918b0b34df22066ab126bd
Sonja Parsell <hello@sparsell.com>
2020-03-20 14:17:17 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,11 @@
 def starts_with_a_vowel?(word)
-  #{}/[aeiou]/.match(word)
   if /[aeiou]/.match(word)
   else false
 end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  /^un (ing$)/.match(text)
 end
 
 def words_five_letters_long(text)
